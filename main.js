109390bbd7c55052eded324aec3c245c2fd2a63e
VerteDinde <keeley.goes.to.work@gmail.com>
2016-12-03 18:37:18 -0800

    Cleaned up old snippets
    Cleaned up some old comments, template code no longer being used

--- main.js
+++ main.js
@@ -42,21 +42,10 @@ if( !cfg.init() ) {
 // configure (initialize) our I/O pins for usage (gives us an I/O object)
 // configuration is based on parameters provided by the call to cfg.init()
 
-cfg.io = new cfg.mraa.Gpio(cfg.ioPin, cfg.ioOwner, cfg.ioRaw) ;
-cfg.io.dir(cfg.mraa.DIR_OUT) ;                  // configure the LED gpio as an output
+cfg.io = new cfg.mraa.Gpio(cfg.ioPin, cfg.ioOwner, cfg.ioRaw);
+cfg.io.dir(cfg.mraa.DIR_OUT);                  // configure the LED gpio as an output
 
-console.log("Using LED pin number: " + cfg.ioPin) ;
-
-
-//// now we are going to flash the LED by toggling it at a periodic interval
-//
-//var ledState ;
-//var periodicActivity = function() {
-//    ledState = cfg.io.read() ;                  // get the current state of the LED pin
-//    cfg.io.write(ledState?0:1) ;                // if the pin is currently 1 write a '0' (low) else write a '1' (high)
-//    process.stdout.write(ledState?'0':'1') ;    // and write an unending stream of toggling 1/0's to the console
-//} ;
-//var intervalID = setInterval(periodicActivity, 1000) ;  // start the periodic toggle
+console.log("Using LED pin number: " + cfg.ioPin);
 
 //Sound Sensor code snippet START
 var sensorObj = require('jsupm_loudness');
@@ -71,7 +60,6 @@ var greenLed = new groveSensor.GroveLed(3);
 //greenLed.on();
 var redLed = new groveSensor.GroveLed(2);
 var isLoud = false;
-
 //redLed.off();
 var blueLed = new groveSensor.GroveLed(4);
 
